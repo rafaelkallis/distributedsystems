cedf12af085851108a5a5e412984f3c6b2e1f895
Rafael Kallis <rafael@rafaelkallis.com>
2015-10-06 07:51:26 +0200

    Added Listener.java functions

--- src/Assignment1/Listener.java
+++ src/Assignment1/Listener.java
@@ -14,13 +14,29 @@ public class Listener
    {
       String serverName = args[0];
       int port = Integer.parseInt(args[1]);
-      DataInputStream streamIn ;
-      DataOutputStream streamOut;
+      DataInputStream in ;
+      DataOutputStream out;
+      Socket client;
       
-	  // create connection to server
-	  // send the string  "LISTENER" to server first!!
-	  // continuously receive messages from server
-      // using stdout to print out messages Received
-      // do not close the connection- keep listening to further messages from the server.
+      try{
+    	  // create connection to server
+          client = new Socket(serverName,port);
+          
+    	  // send the string  "LISTENER" to server first!!
+          out = new DataOutputStream(client.getOutputStream());
+          out.writeUTF("LISTENER");
+          
+    	  // continuously receive messages from server
+          // using stdout to print out messages Received
+          // do not close the connection- keep listening to further messages from the server.
+          in = new DataInputStream(client.getInputStream());
+          while(true){
+        	  System.out.println(in.readUTF());
+          }
+      }catch (UnknownHostException e){
+    	  System.err.println("Host "+serverName+" unknown");
+      }catch (IOException e){
+    	  e.printStackTrace();
+      }
    }
 }
